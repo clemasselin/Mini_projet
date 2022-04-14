52cb9fcecb109d64c919d0e0d4f3f522de2fba19
clemasselin1999@gmail.com <2506Mille996!>
2022-04-14 11:57:55 +0200

    modification perso

--- main.c
+++ main.c
@@ -21,7 +21,7 @@
 
 //uncomment to use double buffering to send the FFT to the computer
 #define DOUBLE_BUFFERING
-
+int var = 0;
 static void serial_start(void)
 {
 	static SerialConfig ser_cfg = {

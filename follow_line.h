ee6c5d1b7165c5e7bd40b78d2f7b870419a78810
marg <margueritebenoist@gmail.com>
2022-04-28 11:53:46 +0200

    follow line update 3

---
+++ follow_line.h
@@ -0,0 +1,15 @@
+/*
+ * follow_line.h
+ *
+ *  Created on: 24 avr. 2022
+ *      Author: margu
+ */
+
+#ifndef FOLLOW_LINE_H_
+#define FOLLOW_LINE_H_
+
+
+void follow_line_start(void);
+
+
+#endif /* FOLLOW_LINE_H_ */

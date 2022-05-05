5d9913d780d9964c55e78252ca7bda6e72fa3782
marg <margueritebenoist@gmail.com>
2022-05-05 12:48:35 +0200

    add assess_environment

---
+++ assess_environment.h
@@ -0,0 +1,9 @@
+#ifndef ASSESS_ENVIRONMENT_H
+#define ASSESS_ENVIRONMENT_H
+
+
+uint16_t get_line_position(void);
+uint8_t get_line_number(void);
+void assess_environment_start(void);
+
+#endif /* ASSESS_ENVIRONMENT_H */
